e08833613365545a7c593f2e2d1ad21fa36f0842
Christopher Campbell <campbefs@gmail.com>
2021-07-19 00:21:32 -0700

    added gestalt

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Button } from 'semantic-ui-react';
+import { Button as Button2 } from 'gestalt';
 
 function App() {
   return (
     <>
       <Button primary>Primary</Button>
       <Button secondary>Secondary</Button>
+      <Button2 text="Medium-sized button" />
     </>
   );
 }
