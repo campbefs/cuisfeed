619bdbf3b981f66a68a6eb3817fa538dc65cc228
Christopher Campbell <campbefs@gmail.com>
2021-07-30 00:17:03 -0700

    nick link

--- client/src/components/ProfileCard/index.js
+++ client/src/components/ProfileCard/index.js
@@ -8,7 +8,7 @@ export default function CardExample() {
     <Box maxWidth={236} padding={2} column={12} margin={8}>
       <Card image={<Avatar name="Nicholas" src="https://i.pinimg.com/originals/bd/35/1e/bd351eff6c29b993ec26ccd9545c8d1c.jpg" />}>
         <Text align="center" weight="bold">
-          <Link href="https://pinterest.com">
+          <Link href="https://www.cinemablend.com/news/2489863/the-most-nicolas-cage-movies-nicolas-cage-has-ever-done">
             <Box paddingX={3} paddingY={2}>
               Nicholas
             </Box>
